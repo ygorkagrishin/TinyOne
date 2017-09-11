e5fab0bf857b5599532233eb1fe36bb04023b2f1
ygorkagrishin <webygorkagrishin@gmail.com>
2017-09-11 15:04:43 +0300

    carousel

--- dist/js/common.js
+++ dist/js/common.js
@@ -16,3 +16,34 @@ if ( section.children[0].offsetHeight >= window.innerHeight ) {
 
 document.addEventListener( 'DOMContentLoaded', fullHeight );
 window.addEventListener( 'resize', fullHeight );
+
+// var carousel = ( function () {
+
+// var carousel = document.querySelector( '.test__carousel' ),
+//     wrap = document.querySelector( '.test-carousel__wrap' ),
+//     slides = document.querySelectorAll( '.test-carousel__slide' ),
+//     slide = document.querySelector( '.test-carousel__slide' ),
+//     counter = slides.length,
+//     interval = 3000;
+
+// var start = 0;
+
+// var timerID = setTimeout( function () {
+
+// var intevalID = setInterval( function () {
+
+//     if ( counter - 1 >= 1 ) {
+//         counter--;
+//         start = start - slide.offsetWidth; }
+//     else if ( counter === 1 ) {
+//         counter = slides.length;
+//         start = 0;
+//     }    
+
+//     wrap.style.transform = 'translateX( ' +  start + 'px' +' )';
+
+//     },interval);
+
+// },interval);
+
+// })();
