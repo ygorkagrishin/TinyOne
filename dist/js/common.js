d9161c1c27a4b2e35142f20ceceb2d98f199b46f
ygorkagrishin <webygorkagrishin@gmail.com>
2017-09-11 17:00:54 +0300

    carousel

--- dist/js/common.js
+++ dist/js/common.js
@@ -17,33 +17,33 @@ if ( section.children[0].offsetHeight >= window.innerHeight ) {
 document.addEventListener( 'DOMContentLoaded', fullHeight );
 window.addEventListener( 'resize', fullHeight );
 
-// var carousel = ( function () {
+var carousel = ( function () {
 
-// var carousel = document.querySelector( '.test__carousel' ),
-//     wrap = document.querySelector( '.test-carousel__wrap' ),
-//     slides = document.querySelectorAll( '.test-carousel__slide' ),
-//     slide = document.querySelector( '.test-carousel__slide' ),
-//     counter = slides.length,
-//     interval = 3000;
+var carousel = document.querySelector( '.test__carousel' ),
+    wrap = document.querySelector( '.test-carousel__wrap' ),
+    slides = document.querySelectorAll( '.test-carousel__slide' ),
+    slide = document.querySelector( '.test-carousel__slide' ),
+    counter = slides.length,
+    interval = 3000;
 
-// var start = 0;
+var start = 0;
 
-// var timerID = setTimeout( function () {
+var timerID = setTimeout( function () {
 
-// var intevalID = setInterval( function () {
+var intevalID = setInterval( function () {
 
-//     if ( counter - 1 >= 1 ) {
-//         counter--;
-//         start = start - slide.offsetWidth; }
-//     else if ( counter === 1 ) {
-//         counter = slides.length;
-//         start = 0;
-//     }    
+    if ( counter - 1 >= 1 ) {
+        counter--;
+        start = start - slide.offsetWidth; }
+    else if ( counter === 1 ) {
+        counter = slides.length;
+        start = 0;
+    }    
 
-//     wrap.style.transform = 'translateX( ' +  start + 'px' +' )';
+    wrap.style.transform = 'translateX( ' +  start + 'px' +' )';
 
-//     },interval);
+    },interval);
 
-// },interval);
+},interval);
 
-// })();
+})();
