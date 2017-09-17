b90a2a2059e3fbfdbf747cea651bc63da5efed5c
ygorkagrishin <webygorkagrishin@gmail.com>
2017-09-17 20:15:25 +0300

    carousel

--- app/js/jScript/carousel.js
+++ app/js/jScript/carousel.js
@@ -1,13 +1,12 @@
-var carousel = ( function () {
+function carousel() {
 
 var carousel = document.querySelector( '.test__carousel' ),
     wrap = document.querySelector( '.test-carousel__wrap' ),
     slides = document.querySelectorAll( '.test-carousel__slide' ),
     slide = document.querySelector( '.test-carousel__slide' ),
     counter = slides.length,
-    interval = 3000;
-
-var start = 0;
+    interval = 3000,
+    start = 0;
 
 var timerID = setTimeout( function () {
 
@@ -15,16 +14,33 @@ var intevalID = setInterval( function () {
 
     if ( counter - 1 >= 1 ) {
         counter--;
+        wrap.classList.add( 'active' );
         start = start - slide.offsetWidth; }
     else if ( counter === 1 ) {
+        wrap.classList.remove( 'active' );
         counter = slides.length;
         start = 0;
     }    
-
+    
     wrap.style.transform = 'translateX( ' +  start + 'px' +' )';
 
     },interval);
 
-},interval);
+},interval); 
 
-})();
+setWidthforContainer();
+
+};
+
+function setWidthforContainer() {
+
+var wrap = document.querySelector( '.test-carousel__wrap' ),
+    slide = document.querySelector( '.test-carousel__slide' ),
+    children = wrap.children.length;
+
+wrap.style.width = slide.offsetWidth * children + 'px';  
+
+};
+
+document.addEventListener( 'DOMContentLoaded', carousel );
+window.addEventListener( 'resize', setWidthforContainer );
